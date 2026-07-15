5a8ca56a686a794a8f8e07d187b621090e8a5cd4
Victor Romero <victor.romero@fintecheando.mx>
2026-07-15 13:06:56 -0600

    MX-375: Improve Transfer to Third Party for Fast Payment Systems

--- src/main/java/org/apache/fineract/selfservice/account/service/PinExternalTransferService.java
+++ src/main/java/org/apache/fineract/selfservice/account/service/PinExternalTransferService.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class SelfServiceExternalTransferService {
+public class PinExternalTransferService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
