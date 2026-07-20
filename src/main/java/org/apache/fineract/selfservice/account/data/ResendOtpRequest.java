495c083ea40edf1dbf7b8e325a2c74c4819f8739
Victor Romero <victor.romero@fintecheando.mx>
2026-07-20 17:07:14 -0600

    MX-375: Improve Transfer to Third Party for Fast Payment Systems

---
+++ src/main/java/org/apache/fineract/selfservice/account/data/ResendOtpRequest.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright since 2026 Mifos Initiative
+ *
+ * <p>This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+ * of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.apache.fineract.selfservice.account.data;
+
+import lombok.Data;
+
+@Data
+public class ResendOtpRequest {
+  // Optional context for resend (e.g., to match transfer session)
+  private String fromAccount;
+  private String toAccount;
+  private String transferType; // e.g., SAME_BANK, SINPE_MOVIL, etc.
+  // Can be extended with transferId if needed for future tracking
+}
